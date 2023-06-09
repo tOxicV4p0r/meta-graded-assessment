b48ee1c40c2602fee3fe32877035297b66504b28
kraisorn <22193826+tOxicV4p0r@users.noreply.github.com>
2023-06-10 05:38:05 +0700

    validate form

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,31 @@
-import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { Button, Card, CardBody, CardFooter, Heading, Image, Stack, Text } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
-const Card = ({ title, description, imageSrc }) => {
-  // Implement the UI for the Card component according to the instructions.
-  // You should be able to implement the component with the elements imported above.
-  // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+const CardComp = ({ title, description, imageSrc }) => {
+    // Implement the UI for the Card component according to the instructions.
+    // You should be able to implement the component with the elements imported above.
+    // Feel free to import other UI components from Chakra UI if you wish to.
+    return (
+        <Card>
+            <Image
+                objectFit='cover'
+                src={imageSrc}
+                alt={title}
+                borderRadius='md'
+            />
+            <CardBody>
+                <Stack>
+                    <Heading size='md'>{title}</Heading>
+                    <Text>{description}</Text>
+                </Stack>
+            </CardBody>
+            <CardFooter>
+                <Text>See more <FontAwesomeIcon icon={faArrowRight} /></Text>
+            </CardFooter>
+        </Card>
+    );
 };
 
-export default Card;
+export default CardComp;
--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useFormik } from "formik";
+import { Formik } from "formik";
 import {
     Box,
     Button,
@@ -17,15 +17,35 @@ import FullScreenSection from "./FullScreenSection";
 import useSubmit from "../hooks/useSubmit";
 import { useAlertContext } from "../context/alertContext";
 
+const contactScheme = Yup.object().shape({
+    firstName: Yup.string().required("Required"),
+    email: Yup.string().email("invalid email").required("Required"),
+    type: Yup.string().required("Required"),
+    comment: Yup.string().required("Required"),
+});
+
+const initialValuesContact = {
+    firstName: '',
+    email: '',
+    type: '',
+    comment: '',
+}
+
 const LandingSection = () => {
     const { isLoading, response, submit } = useSubmit();
     const { onOpen } = useAlertContext();
 
-    const formik = useFormik({
-        initialValues: {},
-        onSubmit: (values) => { },
-        validationSchema: Yup.object({}),
-    });
+
+    const handleFormSubmit = async (values, event) => {
+        console.log(values)
+        // console.log(event)
+        await submit('', values)
+        console.log(response);
+        const { type = 'error', message = '' } = response
+        onOpen(type, message);
+        if (type === 'success')
+            event.resetForm();
+    }
 
     return (
         <FullScreenSection
@@ -39,49 +59,83 @@ const LandingSection = () => {
                     Contact me
                 </Heading>
                 <Box p={6} rounded="md" w="100%">
-                    <form>
-                        <VStack spacing={4}>
-                            <FormControl isInvalid={false}>
-                                <FormLabel htmlFor="firstName">Name</FormLabel>
-                                <Input
-                                    id="firstName"
-                                    name="firstName"
-                                />
-                                <FormErrorMessage></FormErrorMessage>
-                            </FormControl>
-                            <FormControl isInvalid={false}>
-                                <FormLabel htmlFor="email">Email Address</FormLabel>
-                                <Input
-                                    id="email"
-                                    name="email"
-                                    type="email"
-                                />
-                                <FormErrorMessage></FormErrorMessage>
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel htmlFor="type">Type of enquiry</FormLabel>
-                                <Select id="type" name="type">
-                                    <option value="hireMe">Freelance project proposal</option>
-                                    <option value="openSource">
-                                        Open source consultancy session
-                                    </option>
-                                    <option value="other">Other</option>
-                                </Select>
-                            </FormControl>
-                            <FormControl isInvalid={false}>
-                                <FormLabel htmlFor="comment">Your message</FormLabel>
-                                <Textarea
-                                    id="comment"
-                                    name="comment"
-                                    height={250}
-                                />
-                                <FormErrorMessage></FormErrorMessage>
-                            </FormControl>
-                            <Button type="submit" colorScheme="purple" width="full">
-                                Submit
-                            </Button>
-                        </VStack>
-                    </form>
+                    <Formik
+                        initialValues={initialValuesContact}
+                        validationSchema={contactScheme}
+                        onSubmit={handleFormSubmit}
+                    >
+                        {({
+                            values,
+                            errors,
+                            touched,
+                            handleChange,
+                            handleBlur,
+                            handleSubmit,
+                            isSubmitting,
+                            /* and other goodies */
+                        }) => (
+                            <form onSubmit={handleSubmit}>
+                                <VStack spacing={4}>
+                                    <FormControl isInvalid={Boolean(touched.firstName) && Boolean(errors.firstName)}>
+                                        <FormLabel htmlFor="firstName">Name</FormLabel>
+                                        <Input
+                                            id="firstName"
+                                            name="firstName"
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            value={values.firstName}
+                                        />
+                                        <FormErrorMessage>{errors.firstName}</FormErrorMessage>
+                                    </FormControl>
+                                    <FormControl isInvalid={Boolean(touched.email) && Boolean(errors.email)}>
+                                        <FormLabel htmlFor="email">Email Address</FormLabel>
+                                        <Input
+                                            id="email"
+                                            name="email"
+                                            type="email"
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            value={values.email}
+                                        />
+                                        <FormErrorMessage>{errors.email}</FormErrorMessage>
+                                    </FormControl>
+                                    <FormControl isInvalid={Boolean(touched.type) && Boolean(errors.type)}>
+                                        <FormLabel htmlFor="type">Type of enquiry</FormLabel>
+                                        <Select
+                                            id="type"
+                                            name="type"
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            value={values.type}
+                                        >
+                                            <option value="">Please select</option>
+                                            <option value="hireMe">Freelance project proposal</option>
+                                            <option value="openSource">
+                                                Open source consultancy session
+                                            </option>
+                                            <option value="other">Other</option>
+                                        </Select>
+                                        <FormErrorMessage>{errors.type}</FormErrorMessage>
+                                    </FormControl>
+                                    <FormControl isInvalid={Boolean(touched.comment) && Boolean(errors.comment)}>
+                                        <FormLabel htmlFor="comment">Your message</FormLabel>
+                                        <Textarea
+                                            id="comment"
+                                            name="comment"
+                                            height={250}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            value={values.comment}
+                                        />
+                                        <FormErrorMessage>{errors.comment}</FormErrorMessage>
+                                    </FormControl>
+                                    <Button type="submit" colorScheme="purple" width="full" isLoading={isLoading} disabled={isSubmitting}>
+                                        Submit
+                                    </Button>
+                                </VStack>
+                            </form>
+                        )}
+                    </Formik>
                 </Box>
             </VStack>
         </FullScreenSection>
